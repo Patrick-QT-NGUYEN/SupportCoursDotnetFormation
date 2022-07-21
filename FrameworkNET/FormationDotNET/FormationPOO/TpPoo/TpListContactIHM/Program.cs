8dce1d788e479e216e93d1917ad2e824cba33f37
Patrick NGUYEN <p.nguyen@live.fr>
2022-07-21 12:10:56 +0200

    TP POO Liste Contact

---
+++ FrameworkNET/FormationDotNET/FormationPOO/TpPoo/TpListContactIHM/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using TpListContactIHM.Classes;
+
+//Contact c = new();
+//Person p = new();
+//Address a = new();
+
+//string chaine = "";
+//MyRegex.IsPhone(chaine);
+
+
+IHM ihm = new();
+ihm.Start();
