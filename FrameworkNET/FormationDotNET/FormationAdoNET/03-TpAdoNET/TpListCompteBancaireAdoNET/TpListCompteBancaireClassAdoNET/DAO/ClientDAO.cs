472cfc74a44be78c85cacd8036b563927b96d03a
Patrick NGUYEN <p.nguyen@live.fr>
2022-07-29 16:03:14 +0200

    Exercice Ado.NET

---
+++ FrameworkNET/FormationDotNET/FormationAdoNET/03-TpAdoNET/TpListCompteBancaireAdoNET/TpListCompteBancaireClassAdoNET/DAO/ClientDAO.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Data.SqlClient;
+using TpListCompteBancaireClassAdoNET.Classes;
+using TpListCompteBancaireClassAdoNET.Tools;
+
+namespace TpListCompteBancaireClassAdoNET.DAO
+{
+    internal class ClientDAO : BaseDAO<Client>
+    {
+        public override int Create(Client element)
+        {
+            // Redaction de la requete
+            _request = "INSERT INTO CLIENT (NOM , PRENOM, TELEPHONE) OUTPUT INSERTED.ID VALUES (@Nom, @Prenom, @Telephone)";
+            // Création de notre nouvelle instance de connection
+            _connection = Connection.New;
+            // Création de notre objet command
+            _command = new SqlCommand(_request,_connection);
+            // Ajout des params à la requetes
+            _command.Parameters.Add(new SqlParameter("@Nom", element.Nom));
+            _command.Parameters.Add(new SqlParameter("@Prenom", element.Prenom));
+            _command.Parameters.Add(new SqlParameter("@Telephone", element.Telephone));
+            // Ouverture de la connection
+            _connection.Open();
+            // Execution de la commande avec execute scalar ( retourne la valeur d'un champ => ici ID)
+            element.Id = (int)_command.ExecuteScalar();
+            // libération de note objet commande
+            _command.Dispose();
+            // fermeture de la connection
+            _connection.Close();
+            return element.Id;
+        }
+
+        public override bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Delete(Client element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Client Find(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Client> Find(Func<Client, bool> criteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Client> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Update(Client element)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FrameworkNET/FormationDotNET/FormationAdoNET/03-TpAdoNET/TpListCompteBancaireAdoNET/TpListCompteBancaireClassAdoNET/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using TpListCompteBancaireClassAdoNET.Classes;
+using TpListCompteBancaireClassAdoNET.DAO;
+
+#region Test Ajout d'un client
+ClientDAO _clientDAO = new();
+
+Client c = new Client("Di Persio", "Anthony", "+33 6 41 52 63 98");
+_clientDAO.Create(c);
+#endregion
+
+
+
+
+
+
+
+
+
+
+
+
+Console.WriteLine("à Bientôt...");
