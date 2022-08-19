9c848b4bd97818a8ffbe6c5374786b7e77b56a77
Patrick NGUYEN <p.nguyen@live.fr>
2022-08-19 16:29:04 +0200

    Exo eLibrary

---
+++ FrameworkNET/CoursEFCore/EXO_eLibrary/Datas/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using EXO_eLibrary.Classes;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXO_eLibrary.Datas
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Editor> Editors { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public ApplicationDbContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source = (LocalDb)\MSSQLLOCALDB; Initial Catalog = PRF2022_eLibrary; Integrated Security = True;");
+        }
+    }
+}
---
+++ FrameworkNET/CoursEFCore/EXO_eLibrary/Datas/AuthorsRepository.cs
@@ -0,0 +1,50 @@
+﻿using EXO_eLibrary.Classes;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXO_eLibrary.Datas
+{
+    internal class AuthorsRepository : BaseRepository, IRepository<Author>
+    {
+        public bool Add(Author element)
+        {
+            _context.Authors.Add(element);
+
+            return _context.SaveChanges() == 1;
+        }
+
+        public bool Delete(Author element)
+        {
+            if (element != null)
+            {
+                _context.Authors.Remove(element);
+            }
+
+            return _context.SaveChanges() > 0;
+        }
+
+        public ICollection<Author> GetAll()
+        {
+            return _context.Authors.Include(x => x.Books).ToList();
+        }
+
+        public Author GetById(int id)
+        {
+            return _context.Authors.Include(x => x.Books).FirstOrDefault(x => x.Id == id);
+        }
+
+        public bool Update(Author element)
+        {
+            if (element != null)
+            {
+                _context.Authors.Update(element);
+            }
+
+            return _context.SaveChanges() > 0;
+        }
+    }
+}
