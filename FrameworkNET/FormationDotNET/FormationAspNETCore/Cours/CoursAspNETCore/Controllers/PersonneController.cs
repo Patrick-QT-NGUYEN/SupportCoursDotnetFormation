8dbffe01f9ffa1631e0a05bcb537a227eabedf13
Patrick NGUYEN <p.nguyen@live.fr>
2022-08-29 16:59:52 +0200

    Cours ASPNet Core

---
+++ FrameworkNET/FormationDotNET/FormationAspNETCore/Cours/CoursAspNETCore/Controllers/PersonneController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CoursAspNETCore.Models;
+
+namespace CoursAspNETCore.Controllers
+{
+    public class PersonneController : Controller
+    {
+        // GET: PersonneController
+        public ActionResult Index()
+        {
+            ViewData["Title"] = "Personne Index";
+            ViewBag.data = new Personne()
+            {
+                Nom = "Sinclar",
+                Prenom = "Bob",
+                Email = "bob@gmail.com"
+            };
+            Personne.Personnes = new();
+            Personne p = new()
+            {
+                Nom = "Di Persio",
+                Prenom = "Anthony",
+                Email = "anthony@utopios.net",
+            };
+            Personne.Personnes.Add(p);
+            p = new()
+            {
+                Nom = "Abadi",
+                Prenom = "Ihab",
+                Email = "ihab@utopios.net",
+            };
+            Personne.Personnes.Add(p);
+            p = new()
+            {
+                Nom = "Abadi",
+                Prenom = "Marine",
+                Email = "marine@utopios.net",
+            };
+            Personne.Personnes.Add(p);
+            return View(Personne.Personnes);
+        }
+
+        // GET: PersonneController/Details/5
+        public ActionResult Details(int id)
+        {
+            Personne p = new();
+            p = Personne.Find(id);
+            return View(p);
+        }
+
+        // GET: PersonneController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PersonneController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PersonneController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PersonneController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PersonneController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PersonneController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ FrameworkNET/FormationDotNET/FormationAspNETCore/Cours/CoursAspNETCore/Models/Personne.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CoursAspNETCore.Models
+{
+    public class Personne
+    {
+        private static int counter = 0;
+        private int id;
+        private string nom;
+        private string prenom;
+        private string email;
+        private static ObservableCollection<Personne> personnes;
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+        public string Email { get => email; set => email = value; }
+        public int Id { get => id; set => id = value; }
+        public static ObservableCollection<Personne> Personnes { get => personnes; set => personnes = value; }
+
+        public Personne()
+        {
+            Id = ++counter;
+        }
+        public override string ToString()
+        {
+            return $"Id : {Id} - Nom : {Nom}, Prénom : {Prenom} - Email : {Email}";
+        }
+
+        public static Personne Find(int index)
+        {
+            return Personnes[index-1];
+        }
+
+        public void Add()
+        {
+            Personnes.Add(this);
+        }
+    }
+}
